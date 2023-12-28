d9d4911cb2ad37e3f138a2f68c3fb52594a38c25
Eric Axel Andersen <eric.axel@greydataexplorer.dev>
2023-12-28 15:38:36 -0500

    Add garmin console app for local testing

---
+++ Garmin.Connect.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using Garmin.Connect;
+using Garmin.Connect.Auth;
+
+var login = "username";
+var password = "password";
+var authParameters = new BasicAuthParameters(login, password);
+
+var client = new GarminConnectClient(new GarminConnectContext(new HttpClient(), authParameters));
+
+var profile = await client.GetSocialProfile();
+
+Console.WriteLine(profile.GarminGuid);
